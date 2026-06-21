ed3ed62cc4f1351094fe1cb0b6ad9f0f94797534
Quantum <aburorhambagadaniel@gmail.com>
2026-06-21 16:11:04 +0100

    add active heading

--- Quantum-Ecommerce/src/components/Header.jsx
+++ Quantum-Ecommerce/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import App from '../App.jsx'
 import myLogo from "../assets/logo.png"
 import '../App.css';
 function Header({ cart, setCart }) {
-
+  const [change, setChange] = useState('Home');
 
   return (
 
@@ -16,14 +17,14 @@ function Header({ cart, setCart }) {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active text-white brand-name" aria-current="page" to="/">QuantumShop</Link>
+              <li className={`nav-item `} onClick={() => { setChange('Home') }}>
+                <Link className={`nav-link active text-white brand-name  ${change === 'Home' ? 'color' : ''} `} aria-current="page" to="/">QuantumShop</Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="orders" >Orders</Link>
+              <li className="nav-item" onClick={() => { setChange('Order') }}>
+                <Link className={`nav-link text-white ${change === 'Order' ? 'color' : ''}`} to="orders" >Orders</Link>
               </li>
-              <li className="nav-item dropdown">
-                <Link className="nav-link text-white" to="cart">
+              <li className="nav-item dropdown" onClick={() => { setChange('Cart') }}>
+                <Link className={`nav-link text-white" to="cart ${change === 'Cart' ? 'color' : ''}`}>
                   <img src="/src/assets/cart-icon.png" alt="cart-icon" height="20px" />
                   <span className="badge  position-absolute top-1 start-50 translate-middle text-white">
                     {cart}
